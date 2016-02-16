8991b1649d783137f6954fd555edbae1edbbe6e2
trollepierre <trolle.pierre@gmail.com>
2016-02-16 15:23:34 +0100

    Imports Filezilla

---
+++ contact.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link rel="shortcut icon" href="icon.ico">
+
+	<title>Let's stay in touch!</title>
+
+	<!-- Bootstrap core CSS -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- Custom styles for this template http://getbootstrap.com/examples/signin/-->
+	<link href="css/signin.css" rel="stylesheet">
+
+	<!-- Just for debugging purposes. Don't actually copy this line! -->
+	<!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+  	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+  	<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	<![endif]-->
+  </head>
+
+  <body>
+
+	<div class="container col-lg-6 col-lg-offset-2">
+	
+  	<form class="form-horizontal"> 
+		<h1> Let's keep in Touch </h1>
+			<fieldset>  
+				<div class="form-group">  
+					<label class="control-label" for="firstname">First Name</label>  
+					<div class="controls">  
+						<input type="text" class="form-control" id="firstname">  
+					</div>  
+				</div>  
+				<div class="form-group">  
+					<label class="control-label" for="lastname">Last Name</label>  
+					<div class="controls">  
+						<input type="text" class="form-control" id="lastname">  
+					</div>  
+				</div>  
+				<div class="form-group">  
+					<label class="control-label" for="email">Email</label>  
+					<div class="controls">  
+						<input type="email" class="form-control" id="email">  
+					</div>  
+				</div> 
+				<div class="form-group">  
+					<label class="control-label" for="subject">Subject</label>  
+					<div class="controls">  
+						<input type="text" class="form-control" id="subject">  
+					</div>  
+				</div>  						
+				<div class="form-group">  
+					<label class="control-label" for="message">Message</label>  
+					<div class="controls">  
+						<textarea class="form-control" id="message" rows="5"></textarea>  
+					</div>  
+				</div>  
+				<div class="form-actions">  
+					<button type="submit" class="btn btn-primary">Send</button>  
+					<button class="btn">Cancel</button>  
+				</div>  
+			</div>  
+		</fieldset>  
+	</form>  
+						<!--- See more at: http://www.w3resource.com/twitter-bootstrap/forms-tutorial.php#sthash.PPPMiMDM.dpuf
+						http://www.w3resource.com/twitter-bootstrap/forms-tutorial.php  -->
+						
+
+	</div> <!-- /container -->
+	
+
+	<!-- Bootstrap core JavaScript
+	================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+  </body>
+</html>
+
+
---
+++ index.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+  	  <meta charset="utf-8">
+  	  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  	  <meta name="viewport" content="width=device-width, initial-scale=1">
+  	  <meta name="description" content="">
+  	  <meta name="author" content="">
+  	  <link rel="shortcut icon" href="icon.ico">
+    
+  	  <title>Personal Page of Pierre T.</title>
+    
+  	  <!-- Bootstrap core CSS -->
+  	  <link href="css/bootstrap.min.css" rel="stylesheet">
+  	 
+  	  <!-- Custom styles for this template -->
+  	  <link href="css/carrousel.css" rel="stylesheet">
+	</head>
+    
+	<!-- NAVBAR ================================================== -->
+	<body>
+	<?php include_once("analyticstracking.php") ?>
+  	  <div class="navbar-wrapper">
+  		  <div class="container">
+  			  <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+  				  <div class="container">
+  					  <div class="navbar-header">
+  						  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+  							  <span class="sr-only">Toggle navigation</span>
+  							  <span class="icon-bar"></span>
+  							  <span class="icon-bar"></span>
+  							  <span class="icon-bar"></span>
+  						  </button>
+  						  <a class="navbar-brand" href="#perso">Pierre</a>
+  					  </div>
+  					  <div class="navbar-collapse collapse">
+  						  <ul class="nav navbar-nav">
+  							  <li class="active"><a href="#">Home</a></li>
+  							  <li><a href="http://www.recontact.me">World Trip</a></li>
+  							  <li><a href="http://wekit.recontact.me">WeKiT</a></li>
+  							  <li><a href="http://coincollector.recontact.me">CoinCollector</a></li>
+  							  <li class="dropdown">
+  								  <a href="http://startup.recontact.me" class="dropdown-toggle" data-toggle="dropdown">Startup Ideas<b class="caret"></b></a>
+  								  <ul class="dropdown-menu">
+  									  <li><a href="http://startup.recontact.me">View All</a></li>
+  									  <li><a href="http://startup.recontact.me">Holograms</a></li>
+  									  <li><a href="http://startup.recontact.me">Something else here</a></li>
+  									  <li class="divider"></li>
+  									  <li class="dropdown-header">Help me</li>
+  									  <li><a href="http://startup.recontact.me">Share ideas</a></li>
+  									  <li><a href="http://startup.recontact.me">Apply</a></li>
+  								  </ul>
+  							  </li>
+  							  <li><a href="contact.php">Contact</a></li>
+  						  </ul>
+  					  </div>
+  				  </div>
+  			  </div>
+  	 
+  		  </div>
+  	  </div>
+    
+    
+  	  <!-- Carousel   	  ================================================== -->
+  	  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  		  <!-- Indicators -->
+  		  <ol class="carousel-indicators">
+  			  <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+  			  <li data-target="#myCarousel" data-slide-to="1"></li>
+  			  <li data-target="#myCarousel" data-slide-to="2"></li>
+  		  </ol>
+  		  <div class="carousel-inner">
+  			  <div class="item active">
+  				  
+  				  <div class="container">
+  					  <div class="carousel-caption">
+						  <img class="img-thumbnail" src="img/tdm2.jpg" alt="First slide">
+  						  <h1>One year around the world.</h1>
+  						  <p>Who has never dreamed to do a world trip? Beno&icirc;t and I have decided to realise an adventurous travel around the world and we want to share it with you!</p>
+  						  <p><a class="btn btn-lg btn-primary" href="http://www.recontact.me" role="button">Discover our trip</a></p>
+  					  </div>
+  				  </div>
+  			  </div>
+  			  <div class="item">
+  				  <div class="container">
+  					  <div class="carousel-caption">
+						  <img class="img-thumbnail" src="img/wekit.png" alt="Second slide" >
+  						  <h1>Keep in Touch with your relatives.</h1>
+  						  <p>Life is much more interesting when you can share it with the people that really count for you. Continue to maintain a close and supportive relationship with your relatives depending on your affinity:</p>
+  						  <p><a class="btn btn-lg btn-primary" href="http://wekit.recontact.me" role="button">Discover WeKiT</a></p>
+  					  </div>
+  				  </div>
+  			  </div>
+  			  <div class="item">
+  				 <div class="container">
+  					  <div class="carousel-caption">
+						<img class="img-thumbnail" src="img/r.jpg" alt="Third slide">
+						<h1>Help an entrepreneur.</h1>
+						<p>Let me introduce you my startup ideas. Whatever your interest in these ideas, your feedbacks will contribute to the realisation of them. View the description, comment ideas, recommand friends, apply to concretise an idea with me!</p>
+						<p><a class="btn btn-lg btn-primary" href="http://startup.recontact.me" role="button">View ideas</a></p>
+  					  </div>
+  				  </div>
+  			  </div>
+  		  </div>
+  		  <a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+  		  <a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+  	  </div><!-- /.carousel -->
+    
+    
+    
+  	  <!-- Marketing messaging and featurettes ================================================== -->
+  	  <!-- Wrap the rest of the page in another container to center all the content. -->
+  	    <a id="three"></a>  
+	  <div class="container marketing">
+  
+  	  <!-- Three columns of text below the carousel -->
+  		  <div class="row three">
+		    
+			  <div class="col-lg-4">
+  				  <img class="img-circle" src="img/world.jpg" alt="Generic placeholder image">
+  				  <h2>World Trip</h2>
+  				  <p>Discover the world. That sounds like a dream. <br/>
+   				 For me, it is just a worthy opportunity to take. <br/>
+   				 Get a look to my plans about this world trip.</p>
+  				  <p><a class="btn btn-default" href="http://www.recontact.me" role="button">Let's dream &raquo;</a></p>
+  			  </div><!-- /.col-lg-4 -->
+  			  <div class="col-lg-4">
+  				  <img class="img-circle" src="img/kit.png" alt="Generic placeholder image">
+  				  <h2>WeKiT</h2>
+  				  <p>Keep in Touch with friends is valuable. <br/>
+   				 I developed an App to assist me in getting in touch. <br/>
+   				 One day I pitched this idea & WeKiT was created!</p>
+  				  <p><a class="btn btn-default" href="http://wekit.recontact.me" role="button">Discover WeKiT &raquo;</a></p>
+  			  </div><!-- /.col-lg-4 -->
+  			  <div class="col-lg-4">
+  				  <img class="img-circle" src="img/arg.jpg" alt="Generic placeholder image">
+  				  <h2>Coin Collector</h2>
+  				  <p>Finding unknown coin is exciting. <br/>
+   				 Revealing information about coin, sort it out <br/>
+   				 and become the market place for coin collectors. </p>
+  				  <p><a class="btn btn-default" href="http://coincollector.recontact.me" role="button">Here is the deal &raquo;</a></p>
+  			  </div><!-- /.col-lg-4 -->
+			  
+  		  </div><!-- /.row three -->
+  	 
+  		  <!-- START THE FEATURETTES -->
+  			    
+  		  <hr class="featurette-divider">
+  		   
+  		  <div class="row featurette">
+  			  <a id="perso"></a>  
+   		  <div class="col-md-7">
+  				  <h2 class="featurette-heading">One life to explore the whole world.<br/><span class="text-muted">Travelling is my religion.</span></h2>
+  				  <p class="lead">Trekking in Himalaya.<br/>
+  				  Discovering the Golden Temple of Amritsar, India.<br/>
+  				  Visiting Maribor, Slovenia with friends.<br/>
+  				  Hitchhiking in Nubra Valley, India.</p>
+  			  </div>
+  			  <div class="col-md-5">
+  				  <img class="featurette-image img-responsive" src="img/traveler500.png" alt="Generic placeholder image">
+  			  </div>
+  		  </div>
+  	 
+  		  <hr class="featurette-divider">
+  	 
+  		  <div class="row featurette">
+  			  <div class="col-md-5">
+  				  <img class="featurette-image img-responsive" src="img/entrep500.jpg" alt="Generic placeholder image">
+  			  </div>
+  			  <div class="col-md-7">
+  				  <h2 class="featurette-heading">Changing the world at work.<br/> <span class="text-muted">Have real impact with entrepreneurship.</span></h2>
+  				  <p class="lead">Create business by providing useful services for the world. <br/>Motivation and excitement come with the success of the start up. <br/>Being useful for the world, that's the deal!</p>
+  			  </div>
+  		  </div>
+  	 
+  		  <hr class="featurette-divider">
+  	 
+  		  <div class="row featurette">
+  			  <div class="col-md-7">
+  				  <h2 class="featurette-heading">Sharing fun with people.<br/> <span class="text-muted">Who cares about fun if there is nobody sharing it with you.</span></h2>
+  				  <p class="lead">
+  				      Meeting European in Serbia.
+  				  <br/> Gather BEST France during the Join Board Training.
+  				  <br/> Going out with friends.
+  				  <br/> Inside a team of entrepreneurs.</p>
+  			  </div>
+  			  <div class="col-md-5">
+  				  <img class="featurette-image img-responsive" src="img/FRIENDS500.png" alt="Generic placeholder image">
+  			  </div>
+  		  </div>
+  	 
+			<hr>
+
+  	 
+  		  <!-- /END THE FEATURETTES -->
+  	  </div><!-- /.container -->
+   
+  		  <!-- FOOTER -->
+    <footer>
+  			  <p>Website by Pierre T. <br/> Explore my creation on my <a href="#">website</a></p>
+  			  <p>  			  <a href="#">Back to top</a>  			  </p>
+    </footer>
+  	   </body>
+   
+    
+</html>
+<!-- Bootstrap core JavaScript
+	================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/docs.min.js"></script>
+
+
+
+
